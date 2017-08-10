646a80130d9f60d0d19e886af983cd52328ea90d
vctqs1 <vctqs1@gmail.com>
2017-08-10 10:34:49 +0700

    hihi

---
+++ SocialLogin/Block/Form/Login.php
@@ -0,0 +1,15 @@
+<?php
+namespace TMA\SocialLogin\Block\Form;
+/**
+* 
+*/
+class Login extends \Magento\Customer\Block\Form
+{
+	
+	protected function _prepareLayout()
+	{
+		# code...
+
+	}
+}
+?>
